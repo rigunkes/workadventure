6283ca32c48a4744e04d99e62bfdabfb2b959778
rigunkes <richard.monnik@gmail.com>
2025-08-20 18:18:19 -0700

    Main

---
+++ tilesets/colorset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="colorset" tilewidth="32" tileheight="32" tilecount="13500" columns="108">
+ <image source="../images/colorset.png" width="3478" height="4000"/>
+</tileset>
---
+++ tilesets/rickyfloorplan.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="rickyfloorplan" tilewidth="32" tileheight="32" tilecount="13500" columns="108">
+ <image source="../images/rickyfloorplan.png" width="3478" height="4000"/>
+</tileset>
